934768bf9a1d417461048581da144d0662059ade
Keith Gould <keithmgould@gmail.com>
2023-04-01 14:56:16 -0400

    adds in node-fetch module

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fetch = require("node-fetch");
 
 try {
   const prDescription = core.getInput('pr-description');
